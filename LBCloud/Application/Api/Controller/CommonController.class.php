efb09ff4662326f14083b5e085d53124395ffff7
liangjian <liangjian@bestfu.com>
2016-08-08 15:45:58 +0800

    Integrate AliyunOSS SDK

---
+++ LBCloud/Application/Api/Controller/CommonController.class.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * 基类controller
+ * @author liangjian
+ * @email 15934854815@163.com
+ */
+namespace Api\Controller;
+use Think\Controller;
+
+class CommonController extends Controller
+{
+	
+}
---
+++ LBCloud/Application/Api/Controller/PlayerController.class.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * 播放端接口控制器
+ * @author liangjian
+ * @email 15934854815@163.com
+ */
+namespace Api\Controller;
+
+class PlayerController extends CommonController
+{
+	public function index(){
+		
+	}
+}
