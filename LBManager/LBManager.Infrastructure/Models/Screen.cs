1092c041f7b74aee6c8b9eb5e09ad6cca8799e3b
hardborn <hardborn@live.cn>
2016-08-29 10:27:43 +0800

    添加Screen业务域对象

--- LBManager/LBManager.Infrastructure/Models/Screen.cs
+++ LBManager/LBManager.Infrastructure/Models/Screen.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,12 +9,42 @@ namespace LBManager.Infrastructure.Models
 {
     public class Screen
     {
+        public Screen()
+        {
+
+        }
+
+        [JsonProperty("id")]
         public string Id { get; set; }
+
+        [JsonProperty("name")]
         public string Name { get; set; }
+
+        [JsonProperty("size_x")]
         public int Width { get; set; }
+
+        [JsonProperty("size_y")]
         public int Height { get; set; }
+
+        [JsonProperty("resolu_x")]
         public int PixelsWidth { get; set; }
+
+        [JsonProperty("resolu_y")]
         public int PixelsHeight { get; set; }
 
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("operate")]
+        public string Operate { get; set; }
+
+        [JsonProperty("longitude")]
+        public double Longitude { get; set; }
+
+        [JsonProperty("latitude")]
+        public double Latitude { get; set; }
+
+        [JsonProperty("address")]
+        public string Address { get; set; }
     }
 }
