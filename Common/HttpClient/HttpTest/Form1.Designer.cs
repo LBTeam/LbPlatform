e627e132423a0cbf2311ff39b28ca3f7e94c04a1
K1991 <18292671309@139.com>
2016-08-30 15:40:01 +0800

    Update Upload

--- Common/HttpClient/HttpTest/Form1.Designer.cs
+++ Common/HttpClient/HttpTest/Form1.Designer.cs
@@ -31,6 +31,8 @@
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.txtResult = new System.Windows.Forms.TextBox();
+            this.richTxtControl1 = new CCWin.SkinControl.RichTxtControl();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -59,13 +61,33 @@
             this.txtResult.Size = new System.Drawing.Size(510, 73);
             this.txtResult.TabIndex = 4;
             // 
+            // richTxtControl1
+            // 
+            this.richTxtControl1.Location = new System.Drawing.Point(455, 47);
+            this.richTxtControl1.Name = "richTxtControl1";
+            this.richTxtControl1.Size = new System.Drawing.Size(150, 150);
+            this.richTxtControl1.TabIndex = 5;
+            this.richTxtControl1.Load += new System.EventHandler(this.richTxtControl1_Load);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(411, 79);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "上传";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(510, 252);
+            this.Controls.Add(this.richTxtControl1);
             this.Controls.Add(this.txtResult);
             this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -79,6 +101,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox txtResult;
+        private CCWin.SkinControl.RichTxtControl richTxtControl1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
