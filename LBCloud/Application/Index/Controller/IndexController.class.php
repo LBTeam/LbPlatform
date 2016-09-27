62b7e1b2226f20a05f2b081aaf0c516ea0e6c5c6
liangjian <15934854815@163.com>
2016-09-27 22:53:28 +0800

    Web Manager

--- LBCloud/Application/Index/Controller/IndexController.class.php
+++ LBCloud/Application/Index/Controller/IndexController.class.php
@@ -15,4 +15,81 @@ class IndexController extends CommonController {
     	}
     	$this->success($citys);
     }
+	
+	public function reset_pwd(){
+		if(IS_POST){
+			$user_model = D("User");
+			$rules = array(
+				array('old','require','原密码不能为空！'),
+				array('password','require','新密码不能为空！'),
+				array('repassword','require','确认密码不能为空！'),
+				array('password', "/^[A-Za-z0-9]{6,16}$/", '新密码格式错误！'),
+				array('repassword','password','两次输入密码不一致！',0,'confirm')
+			);
+			if($user_model->validate($rules)->create()){
+				$info = $user_model->user_by_id(ADMIN_UID, 'password');
+				$old = I("post.old");
+				$password = I("post.password");
+				$db_pwd = $info['password'];
+				if(sp_compare_password($old, $db_pwd)){
+					$data = array();
+					$data['uid'] = ADMIN_UID;
+					$data['password'] = sp_password($password);
+					$reset = $user_model->save($data);
+					if($reset !== false){
+						$this->success("密码修改成功！", U('index'));
+					}else{
+						$this->error("密码修改失败！");
+					}
+				}else{
+					$this->error("原密码输入错误！");
+				}
+			}else{
+				$this->error($user_model->getError());
+			}
+		}else{
+			$this->display();
+		}
+	}
+
+	public function information(){
+		if(IS_POST){
+			$user_model = D("User");
+			$rules = array(
+				array('uid','require','用户ID不能为空！'),
+				array('email','require','邮箱不能为空！'),
+				array('email','email','邮箱格式错误！'),
+				array('email','','邮箱已经存在！',0,'unique',2),
+				array('phone','','手机号码已经存在！',2,'unique',2)
+			);
+			if($user_model->validate($rules)->create()){
+				$data = array();
+				$data['uid']		= I("post.uid");
+				$data['email']		= I("post.email");
+				$data['phone']		= I("post.phone", "");
+				$data['realname']	= I("post.realname", "");
+				$data['address']	= I("post.address", "");
+				$res = $user_model->save($data);
+				if($res !== false){
+					$auth = array(
+			            'uid'		=> $data['uid'],
+			            'email'		=> $data['email'],
+			            'lasttime'	=> NOW_TIME
+			        );
+					session('user_auth', $auth);
+        			session('user_auth_sign', data_auth_sign($auth));
+					$this->success('修改成功');
+				}else{
+					$this->error('修改失败');
+				}
+			}else{
+				$this->error($user_model->getError());
+			}
+		}else{
+			$user_model = D("User");
+			$info = $user_model->user_by_id(ADMIN_UID);
+			$this->assign("info", $info);
+			$this->display();
+		}
+	}
 }
