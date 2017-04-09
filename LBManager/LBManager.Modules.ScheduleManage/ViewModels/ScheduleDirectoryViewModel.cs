d21395641aaf5b9d9acff17e36b699668dfa3740
hardborn <hardborn@live.cn>
2017-04-09 21:19:49 +0800

    播放方案列表改为树形展示

---
+++ LBManager/LBManager.Modules.ScheduleManage/ViewModels/ScheduleDirectoryViewModel.cs
@@ -0,0 +1,151 @@
+﻿using LBManager.Modules.ScheduleManage.Views;
+using Prism.Commands;
+using Prism.Logging;
+using Prism.Mvvm;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Windows;
+
+namespace LBManager.Modules.ScheduleManage.ViewModels
+{
+    public class ScheduleDirectoryViewModel : TreeViewItemViewModel
+    {
+        private ILoggerFacade _logger;
+        public ScheduleDirectoryViewModel(DirectoryInfo directoryInfo, TreeViewItemViewModel parent)
+            : base(parent, true)
+        {
+            FetchSchedules(directoryInfo);
+            DirectoryName = Path.GetFileName( directoryInfo.FullName);
+        }
+        private string _directoryName = string.Empty;
+
+        public string DirectoryName
+        {
+            get { return _directoryName; }
+            set { SetProperty(ref _directoryName, value); }
+        }
+
+
+
+        public DelegateCommand NewScheduleFileCommand => new DelegateCommand(NewScheduleFile, CanNewScheduleFile);
+
+        private bool CanNewScheduleFile()
+        {
+            return true;
+        }
+
+        private void NewScheduleFile()
+        {
+            ScheduleView scheduleView = new ScheduleView();
+            scheduleView.Owner = Application.Current.MainWindow;
+            scheduleView.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            scheduleView.DataContext = new ScheduleViewModel();
+            scheduleView.ShowDialog();
+        }
+
+        private DelegateCommand _deleteScheduleDirectoryCommand;
+
+        public DelegateCommand DeleteScheduleCommand
+        {
+            get
+            {
+                if (_deleteScheduleDirectoryCommand == null)
+                {
+                    _deleteScheduleDirectoryCommand = new DelegateCommand(DeleteScheduleDirectory, CanDeleteScheduleDirectory);
+                }
+                return _deleteScheduleDirectoryCommand;
+            }
+        }
+
+        private bool CanDeleteScheduleDirectory()
+        {
+            return true;
+        }
+
+        private void DeleteScheduleDirectory()
+        {
+            //if (File.Exists(CurrentScheduleSummary.FilePath))
+            //{
+            //    File.Delete(CurrentScheduleSummary.FilePath);
+            //}
+            //ScheduleSummaryList.Remove(CurrentScheduleSummary);
+            //if (ScheduleSummaryList.Count > 0)
+            //{
+            //    CurrentScheduleSummary = ScheduleSummaryList[0];
+            //}
+            //else
+            //{
+            //    CurrentScheduleSummary = null;
+            //}
+        }
+
+
+        //private DelegateCommand _fetchBackedUpScheduleCommand;
+        //public DelegateCommand FetchBackedUpScheduleCommand
+        //{
+        //    get
+        //    {
+        //        if (_fetchBackedUpScheduleCommand == null)
+        //        {
+        //            _fetchBackedUpScheduleCommand = new DelegateCommand(FetchBackedUpSchedule, CanFetchBackedUpSchedule);
+        //        }
+        //        return _fetchBackedUpScheduleCommand;
+        //    }
+        //}
+
+        //private DelegateCommand _backupScheduleCommand;
+
+        //public DelegateCommand BackupScheduleCommand
+        //{
+        //    get
+        //    {
+        //        if (_backupScheduleCommand == null)
+        //        {
+        //            _backupScheduleCommand = new DelegateCommand(BackupSchedule, CanBackupSchedule);
+        //        }
+        //        return _backupScheduleCommand;
+        //    }
+        //}
+
+        //private bool CanBackupSchedule()
+        //{
+        //    return CurrentScheduleSummary == null ? false : true;
+        //}
+
+        //private async void BackupSchedule()
+        //{
+        //    // BackupScheduleRequest backupRequest = new BackupScheduleRequest();
+        //    var request = new BackupScheduleRequest();
+        //    request.FileName = CurrentScheduleSummary.FilePath;
+        //    request.FileMD5 = FileUtils.ComputeFileMd5(CurrentScheduleSummary.FilePath);
+
+        //    //string jsonRequest = JsonConvert.SerializeObject(request);
+        //    bool bacupResult = await _scheduleService.BackupSchedules(request);
+        //}
+
+        //private bool CanFetchBackedUpSchedule()
+        //{
+        //    return true;
+        //}
+
+        //private async void FetchBackedUpSchedule()
+        //{
+        //    bool bacupResult = await _scheduleService.GetBackedUpSchedules();
+        //}
+
+        private void FetchSchedules(DirectoryInfo directoryInfo)
+        {
+            //_logger.Log("获取本机播放方案...", Category.Debug, Priority.Medium);
+            //DirectoryInfo folder = new DirectoryInfo(directoryPath);
+
+            foreach (var directory in directoryInfo.GetDirectories())
+            {
+                Children.Add(new ScheduleDirectoryViewModel(directory, this));
+            }
+            foreach (FileInfo file in directoryInfo.GetFiles("*.playprog"))
+            {
+                Children.Add(new ScheduleFileViewModel(file, this));
+            }
+        }
+    }
+}
---
+++ LBManager/LBManager.Modules.ScheduleManage/ViewModels/TreeViewItemViewModel.cs
@@ -0,0 +1,160 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LBManager.Modules.ScheduleManage.ViewModels
+{
+    public class TreeViewItemViewModel : BindableBase
+    {
+        static readonly TreeViewItemViewModel DummyChild = new TreeViewItemViewModel();
+        private ObservableCollection<TreeViewItemViewModel> children;
+        private static ObservableCollection<TreeViewItemViewModel> checkedItem = new ObservableCollection<TreeViewItemViewModel>();
+        readonly TreeViewItemViewModel parent;
+        bool isExpanded;
+        bool isSelected;
+        bool? isChecked = false;
+
+        private TreeViewItemViewModel()
+        {
+        }
+
+        protected TreeViewItemViewModel(TreeViewItemViewModel parent, bool lazyLoadChildren)
+        {
+            this.parent = parent;
+            this.children = new ObservableCollection<TreeViewItemViewModel>();
+
+            if (lazyLoadChildren)
+                this.children.Add(DummyChild);
+        }
+
+        public ObservableCollection<TreeViewItemViewModel> Children
+        {
+            get { return children; }
+            set
+            {
+                children = value;
+                SetProperty(ref children, value);
+            }
+        }
+
+        public bool HasDummyChild
+        {
+            get { return this.Children.Count > 0 && this.Children[0] == DummyChild; }
+        }
+
+        public bool IsExpanded
+        {
+            get { return isExpanded; }
+            set
+            {
+                if (value != isExpanded)
+                {
+                    isExpanded = value;
+                    SetProperty(ref isExpanded, value);
+                }
+
+                if (isExpanded && parent != null)
+                    parent.isExpanded = true;
+
+
+                //if (isExpanded == false && !ProjectAppHelper.IsPublishViewModel)
+                //{
+                //    this.Children = new ObservableCollection<TreeViewItemViewModel>();
+                //    this.Children.Insert(0, DummyChild);
+                //}
+
+                if (this.HasDummyChild && isExpanded == true)
+                {
+                    this.Children.Remove(DummyChild);
+                    this.LoadChildren();
+                }
+            }
+        }
+
+        public bool IsSelected
+        {
+            get { return isSelected; }
+            set
+            {
+                if (value != isSelected)
+                {
+                    isSelected = value;
+                    SetProperty(ref isSelected,value);
+                }
+            }
+        }
+
+        public bool? IsChecked
+        {
+            get { return isChecked; }
+            set { this.SetIsChecked(value, true, true); }
+        }
+
+        void SetIsChecked(bool? value, bool updateChildren, bool updateParent)
+        {
+            //if (value == isChecked)
+            //    return;
+
+            isChecked = value;
+
+            if (updateChildren && isChecked.HasValue)
+            {
+                if (HasDummyChild)
+                {
+                    this.Children.Remove(DummyChild);
+                    this.LoadChildren();
+                }
+                this.Children.ToList().ForEach(c => c.SetIsChecked(isChecked, true, false));
+            }
+
+            if (updateParent && parent != null)
+                parent.VerifyCheckState();
+
+
+            SetProperty(ref isChecked,value);
+
+            //if (value == true || value == null)
+            //{
+            //    if (!this.GetType().Equals(typeof(FeelingFileViewModel)) && this.CheckedItem.Contains(this))
+            //        return;
+            //    else
+            //        this.CheckedItem.Add(this);
+            //}
+            //else
+            //    this.CheckedItem.Remove(this);
+        }
+
+        void VerifyCheckState()
+        {
+            bool? state = null;
+            for (int i = 0; i < this.Children.Count; ++i)
+            {
+                bool? current = this.Children[i].IsChecked;
+                if (i == 0)
+                {
+                    state = current;
+                }
+                else if (state != current)
+                {
+                    state = null;
+                    break;
+                }
+            }
+            this.SetIsChecked(state, false, true);
+        }
+
+        public virtual void LoadChildren()
+        {
+
+        }
+
+        public TreeViewItemViewModel Parent
+        {
+            get { return parent; }
+        }
+    }
+}
